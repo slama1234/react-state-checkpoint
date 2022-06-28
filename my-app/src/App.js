adca3399067df952dfdd89489290004fb3b36ba0
Mustapha Slama <mustaphaslama141@gmail.com>
2022-06-28 12:53:07 +0200

    react state

---
+++ my-app/src/App.js
@@ -0,0 +1,46 @@
+import React from "react"
+import './App.css';
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  this.state = {
+    Person : { fullName:"Mustapha Slama", bio:"this is my react JS", imgSrc:"profile.jpg", profession:"it support "},
+    show : true,
+    count: 0
+  };
+}
+  render(){
+    const count = this.state.count
+    return (
+    <div className="App">
+      {
+        this.state.show?
+      <div>
+        <h1>{this.state.Person.fullName}</h1>
+        <h1>{this.state.Person.bio}</h1>
+        <div><img src={this.state.Person.imgSrc} alt={"me"}style={{width:500}}></img></div>
+        <h1>{this.state.Person.profession}</h1>
+        <h1>Current Count : {count}</h1>
+      </div>
+      :null
+      }
+      <button onClick={()=>this.setState({...this.state,show:!this.state.show})}>Hide/Show</button>
+    </div>)
+  }
+
+  componentDidMount() {
+    this.myInterval = setInterval (()=>{
+      this.setState(prevState => ({
+        count: prevState.count + 1
+    }))
+  }, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.myInterval)
+  }
+
+}
+
+
+export default App;
